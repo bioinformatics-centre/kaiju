1225bb2497b6273531910594a6267449f969a5de
Peter Menzel <pmenzel@gmail.com>
2017-11-22 15:51:00 +0100

    add missing init list for one Fragment ctor

--- src/ConsumerThread.hpp
+++ src/ConsumerThread.hpp
@@ -48,12 +48,12 @@ class Fragment {
 	std::string seq;
 	unsigned int num_mm = 0;
 	int diff = 0;
-	bool SEGchecked = false;
 	unsigned int pos_lastmm = 0;
 	IndexType si0, si1;
 	int matchlen;
+	bool SEGchecked = false;
 	Fragment(const std::string & s) : seq(s) { }
-	Fragment(const std::string & s, bool b) { seq = s; SEGchecked = true; }
+	Fragment(const std::string & s, bool b) : seq(s), SEGchecked(true) { }
 	Fragment(const std::string & s, unsigned int n, unsigned int p, int d) : seq(s), num_mm(n), diff(d), pos_lastmm(p) { }
 	Fragment(const std::string & s, unsigned int n, unsigned int p, int d, IndexType arg_si0, IndexType arg_si1, int len) : seq(s), num_mm(n), diff(d), pos_lastmm(p), si0(arg_si0), si1(arg_si1), matchlen(len), SEGchecked(true) { } // fragments with substitutions have been checked before
 	Fragment(const std::string & s, unsigned int n, unsigned int p, int d, SI * si) : seq(s), num_mm(n), diff(d), pos_lastmm(p), si0(si->start),  si1(si->start+(IndexType)si->len), matchlen(si->ql) { }
