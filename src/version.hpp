591302244a95e8410e5f8df2b95bc21392611e0d
Peter Menzel <pmenzel@gmail.com>
2022-05-12 09:44:11 +0200

    bump version to 1.9.0

--- src/version.hpp
+++ src/version.hpp
@@ -1,9 +1,9 @@
-/* This file is part of Kaiju, Copyright 2015-2021 Peter Menzel and Anders Krogh,
+/* This file is part of Kaiju, Copyright 2015-2022 Peter Menzel and Anders Krogh,
  * Kaiju is licensed under the GPLv3, see the file LICENSE. */
 
 #ifndef KAIJU_VERSION_H
 #define KAIJU_VERSION_H
 
-#define KAIJUVERSION "1.8.2"
+#define KAIJUVERSION "1.9.0"
 
 #endif
